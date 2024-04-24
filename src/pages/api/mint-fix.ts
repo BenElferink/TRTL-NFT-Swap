309ba8127776360aeafbb5c318d36bf9beae9052
Ben Elferink <ben.elferink@icloud.com>
2024-04-24 12:23:13 +0300

    add cron job

---
+++ src/pages/api/mint-fix.ts
@@ -0,0 +1,55 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
+import { firestore } from '@/utils/firebase'
+import sleep from '@/functions/sleep'
+
+export const config = {
+  maxDuration: 300,
+  api: {
+    responseLimit: false,
+  },
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req
+
+  try {
+    switch (method) {
+      case 'GET': {
+        const now = new Date().getTime()
+        const waitTime = 900000
+
+        const collection = firestore.collection('swaps')
+        const { docs } = await collection.get()
+
+        const swaps = docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        })) as (DbPayload & { id: string })[]
+
+        const swapsThatNeedToMint = swaps.filter((doc) => !doc.didMint && !!doc.didBurn && now - doc.timestamp >= waitTime)
+
+        if (swapsThatNeedToMint.length) {
+          console.warn(`found ${swapsThatNeedToMint.length} swaps that need to mint`)
+
+          for await (const { id } of swapsThatNeedToMint) {
+            await axios.post('https://turtle-syndicate-swap.vercel.app/api/mint', { docId: id })
+            await sleep(2000)
+          }
+        }
+
+        return res.status(204).end()
+      }
+
+      default: {
+        res.setHeader('Allow', 'GET')
+        return res.status(405).end()
+      }
+    }
+  } catch (error) {
+    console.error(error)
+    return res.status(500).end()
+  }
+}
+
+export default handler
