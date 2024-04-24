c1d72c68490ef7b522d26ea0540c041e50b9da5a
Ben Elferink <ben.elferink@icloud.com>
2024-04-24 15:41:44 +0300

    add new policy ID

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,7 +5,7 @@ export const DECIMALS = {
 }
 
 export const OLD_POLICY_ID = '263eb3e3c980c15305f393dc7a2f6289ba12732b6636efe46d6e2c16'
-export const NEW_POLICY_ID = '' // ?? 8200581c56bb2614ac2188513faf7770c0dc8b6c7a2b54add30b22a094394da3
+export const NEW_POLICY_ID = '4c1e0a4bcdd31f9e0dcdb62c8e7ce2dc69265078f41663ed8ab66816'
 
 export const TEAM_WALLET_ADDRESS = 'addr1qx77evuery6s5taffnmg72caf0srne8p4lwemv7c45wag39xtcpgwp9h4hnhe4u8rd673uet6qdjvzynpaq8x99nerdqr2sajs'
 export const DEV_WALLET_ADDRESS = 'addr1q9knw3lmvlpsvemjpgmkqkwtkzv8wueggf9aavvzyt2akpw7nsavexls6g59x007aucn2etqp2q4rd0929z2ukcn78fslm56p9'
