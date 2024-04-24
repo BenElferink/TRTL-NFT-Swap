e558685b14f8acaf729563be2c96c9a6bd9ace48
Ben Elferink <ben.elferink@icloud.com>
2024-04-24 13:33:35 +0300

    add team wallet

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,9 +5,9 @@ export const DECIMALS = {
 }
 
 export const OLD_POLICY_ID = '263eb3e3c980c15305f393dc7a2f6289ba12732b6636efe46d6e2c16'
-export const NEW_POLICY_ID = ''
+export const NEW_POLICY_ID = '' // ?? 8200581c56bb2614ac2188513faf7770c0dc8b6c7a2b54add30b22a094394da3
 
-export const TEAM_WALLET_ADDRESS = ''
+export const TEAM_WALLET_ADDRESS = 'addr1qx77evuery6s5taffnmg72caf0srne8p4lwemv7c45wag39xtcpgwp9h4hnhe4u8rd673uet6qdjvzynpaq8x99nerdqr2sajs'
 export const DEV_WALLET_ADDRESS = 'addr1q9knw3lmvlpsvemjpgmkqkwtkzv8wueggf9aavvzyt2akpw7nsavexls6g59x007aucn2etqp2q4rd0929z2ukcn78fslm56p9'
 export const MINT_WALLET_ADDRESS = 'addr1v9ttkfs54sscs5fl4amhpsxu3dk852654hfskg4qjsu5mgcxu36dr'
 export const MINT_WALLET_MNEMONIC = Array.isArray(process.env.MINT_WALLET_MNEMONIC)
