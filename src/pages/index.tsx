738a9ace3d3019c6c52c33b482295e06d879b158
Ben Elferink <ben.elferink@icloud.com>
2024-04-25 08:30:30 +0300

    stop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -125,10 +125,12 @@ const Page = () => {
 
               <p>
                 You have {total} NFTs to trade-in {total ? '🥳' : '😔'}
+                <br />
+                We&apos;ll be online shortly!
               </p>
 
               <div className='my-4'>
-                <Button label='Trade In' disabled={!total || progress.loading || progress.done} onClick={handleTradeIn} />
+                <Button label='Trade In' disabled={true || !total || progress.loading || progress.done} onClick={handleTradeIn} />
               </div>
             </Fragment>
           ) : null}
